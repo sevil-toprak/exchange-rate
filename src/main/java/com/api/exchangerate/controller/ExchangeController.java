ab8678b924aff6011e98a6707d320d2e8ef41433
sevil-toprak <sevil.toprak@hotmail.com>
2025-05-19 00:30:25 +0800

    Merge pull request #2
    * Add basic class and packages

---
+++ src/main/java/com/api/exchangerate/controller/ExchangeController.java
@@ -0,0 +1,11 @@
+package com.api.exchangerate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exchange")
+@RequiredArgsConstructor
+public class ExchangeController {
+}
---
+++ src/main/java/com/api/exchangerate/controller/ExchangeHistoryController.java
@@ -0,0 +1,11 @@
+package com.api.exchangerate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exchange/history")
+@RequiredArgsConstructor
+public class ExchangeHistoryController {
+}
